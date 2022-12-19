1c61321dd3050361ca37d256dea4cf2197fcbdd0
potyoma <artyomnovosyolov254@gmail.com>
2022-12-19 20:41:52 +0400

    fix: operating system functions

--- src/core/os/index.js
+++ src/core/os/index.js
@@ -2,7 +2,16 @@ import os from "os"
 
 const getEol = () => JSON.stringify(os.EOL)
 
-const getCpus = () => os.cpus()
+const getCpus = () => {
+  const res = os.cpus()
+  const { length: count } = res
+  const toGhz = speed => Math.round(speed / 1000)
+  const cpusData = res.reduce(
+    (acc, curr) => acc + `${curr.model} - ${toGhz(curr.speed)} GHz\n`,
+    `Total: ${count}\n`
+  )
+  return cpusData
+}
 
 const getHomedir = () => os.homedir()
 
@@ -10,6 +19,4 @@ const getUsername = () => os.userInfo().username
 
 const getArchitecture = () => os.arch()
 
-const funcs = [getEol, getCpus, getHomedir, getUsername, getArchitecture]
-
-funcs.forEach(f => console.log(f?.()))
+export { getEol, getCpus, getHomedir, getUsername, getArchitecture }
